0afc2dfe5513715b920654dae0e618eb96eafa76
Jess Lee Yarbrough <jess@thewild.blue>
2019-04-12 11:33:15 -0400

    initial commit

---
+++ readme.md
@@ -0,0 +1,64 @@
+# Go JSON Schema Validator Benchmarks (Draft 7 Only)
+
+*April 11, 2019*
+[TheWildBlue](http://github.com/TheWildBlue)
+
+## Systems Under Test
+
+[http://github.com/qri-io/jsonschema]()
+[http://github.com/xeipuuv/gojsonschema]()
+[http://github.com/santhosh-tekuri/jsonschema]()
+
+## Method
+
+* Tested against [http://github.com/json-schema-org/JSON-Schema-Test-Suite/tests/draft7]() except `/optional` and `refRemote.json` (I didn't feel like writing the boilerplate that would allow the remote refs to resolve)
+* Testing for `validate()` *speed*, not correct `validate()` *result*
+* Timed only canonical use of each implementation according to its own docs, not time spent converting input to the appropriate format for each
+
+## Results
+
+* All `qro-io` implementation `validate()` calls succeed with `2` incorrect results and required `3528496 ns` to test all cases 
+* All `xeipuuv` implementation `validate()` calls succeed with `no` incorrect results and required `6731991 ns` to test all cases
+* `santosh-tekuri` implementation failed at or before `validate()` for many cases, so I skipped the offending files. There were `no` incorrect results in cases where `validate()` succeeded, and it took `23028484 ns` to test all cases.
+
+```	
+// from BenchmarkSanthosh(), skipped all cases from these files to pass benchmark
+if s.src == "definitions.json" ||
+    s.src == "ref.json" {
+    continue
+}
+```
+
+```
+// from BenchmarkSanthosh(), skipped all cases from these files to pass benchmark
+if s.src == "const.json" ||
+    s.src == "contains.json" ||
+    s.src == "enum.json" ||
+    s.src == "uniqueItems.json" {
+    continue
+}
+```
+### Raw Results
+
+```
+BenchmarkQri-8        	     500	   3528496 ns/op	 1417909 B/op	   20094 allocs/op
+--- BENCH: BenchmarkQri-8
+    $GOPATH\src\github.com\TheWildBlue\validator-benchmarks\validator_test.go:99: incorrect validate. schema file: definitions.json. schema desc: valid definition. test desc: valid definition schema.
+    $GOPATH\src\github.com\TheWildBlue\validator-benchmarks\validator_test.go:99: incorrect validate. schema file: ref.json. schema desc: remote ref, containing refs itself. test desc: remote ref valid.
+	... [output truncated]
+BenchmarkXeipuu-8     	     200	   6731991 ns/op	 4643779 B/op	   63657 allocs/op
+BenchmarkSanthosh-8   	      50	  23028484 ns/op	 2527688 B/op	   23763 allocs/op
+PASS
+ok  	github.com/TheWildBlue/validator-benchmarks	16.058s
+Success: Benchmarks passed.
+```
+
+## Conclusion
+
+`qri-io` implementation is generally ~2x faster than `xeipuu`. 
+
+`xeipuu` is generally ~3.5x than `santhosh-tekuri`, even with many cases skipped for the latter (thus `qri-io` is generally ~7x faster than `santhosh-tekuri`).
+
+`qri-io` does not validate `2` cases from `definitions.json` and `ref.json` correctly.
+
+`santhosh-tekuri` failed for many cases.
---
+++ validator_test.go
@@ -0,0 +1,174 @@
+package benchmarks
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"path"
+	"testing"
+
+	qri "github.com/qri-io/jsonschema"
+	santhosh "github.com/santhosh-tekuri/jsonschema"
+	xeipuuv "github.com/xeipuuv/gojsonschema"
+)
+
+type Data struct {
+	Description string      `json:"description"`
+	Data        interface{} `json:"data"`
+	Valid       bool        `json:"valid"`
+}
+
+type Schema struct {
+	Description string      `json:"description"`
+	Schema      interface{} `json:"schema"`
+	Tests       []Data      `json:"tests"`
+	src         string
+}
+
+const (
+	dir  = "draft7-tests"
+	temp = ".schema"
+	msg  = "incorrect validate"
+)
+
+var (
+	initErr error
+	schemas = make([]Schema, 0, 64)
+)
+
+func init() {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		initErr = err
+		return
+	}
+	for _, file := range files {
+		if !file.IsDir() {
+			bytes, err := ioutil.ReadFile(path.Join(dir, file.Name()))
+			if err != nil {
+				initErr = err
+				return
+			}
+			var fileSchemas []Schema
+			err = json.Unmarshal(bytes, &fileSchemas)
+			if err != nil {
+				initErr = err
+				return
+			}
+			for _, schema := range fileSchemas {
+				schema.src = file.Name()
+				schemas = append(schemas, schema)
+			}
+		}
+	}
+}
+
+func BenchmarkQri(b *testing.B) {
+	if initErr != nil {
+		b.Fatal(initErr.Error())
+	}
+	for i := 0; i < b.N; i++ {
+		for _, s := range schemas {
+			// local init
+			b.StopTimer()
+			bytes, err := json.Marshal(s.Schema)
+			if err != nil {
+				b.Fatal(err.Error())
+				continue
+			}
+			b.StartTimer()
+			rs := &qri.RootSchema{}
+			err = json.Unmarshal(bytes, rs)
+			if err != nil {
+				b.Fatal(err.Error())
+				continue
+			}
+			for _, test := range s.Tests {
+				b.StopTimer()
+				bytes, err := json.Marshal(test.Data)
+				if err != nil {
+					b.Error(err.Error())
+					continue
+				}
+				b.StartTimer()
+				valErrs, err := rs.ValidateBytes(bytes)
+				if err != nil {
+					b.Error(err.Error())
+					continue
+				}
+				if len(valErrs) > 0 && test.Valid || len(valErrs) == 0 && !test.Valid {
+					b.Logf("%s. schema file: %s. schema desc: %s. test desc: %s.",
+						msg,
+						s.src,
+						s.Description,
+						test.Description)
+				}
+			}
+		}
+	}
+}
+
+func BenchmarkXeipuu(b *testing.B) {
+	if initErr != nil {
+		b.Fatal(initErr.Error())
+	}
+	for i := 0; i < b.N; i++ {
+		for _, s := range schemas {
+			schemaLoader := xeipuuv.NewGoLoader(s.Schema)
+			for _, test := range s.Tests {
+				documentLoader := xeipuuv.NewGoLoader(test.Data)
+				result, err := xeipuuv.Validate(schemaLoader, documentLoader)
+				if err != nil {
+					b.Error(err.Error())
+					continue
+				}
+				if result.Valid() != test.Valid {
+					b.Log(msg)
+				}
+			}
+		}
+	}
+}
+
+func BenchmarkSanthosh(b *testing.B) {
+	if initErr != nil {
+		b.Fatal(initErr.Error())
+	}
+	for i := 0; i < b.N; i++ {
+		for _, s := range schemas {
+			// local init
+			b.StopTimer()
+			bytes, err := json.Marshal(s.Schema)
+			if err != nil {
+				b.Fatal(err.Error())
+			}
+			err = ioutil.WriteFile(temp, bytes, 0644)
+			if err != nil {
+				b.Fatal(err.Error())
+			}
+			b.StartTimer()
+			for _, test := range s.Tests {
+				// Compile() doesn't handle schemas from these well, intercepting to get PASS
+				if s.src == "definitions.json" ||
+					s.src == "ref.json" {
+					continue
+				}
+				schema, err := santhosh.Compile(temp)
+				if err != nil {
+					b.Error(err.Error())
+					continue
+				}
+				// ValidateInterface() doesn't handle data from these well, intercepting to get PASS
+				if s.src == "const.json" ||
+					s.src == "contains.json" ||
+					s.src == "enum.json" ||
+					s.src == "uniqueItems.json" {
+					continue
+				}
+				err = schema.ValidateInterface(test.Data)
+				if err != nil && test.Valid || err == nil && !test.Valid {
+					b.Log(msg)
+				}
+			}
+		}
+	}
+}
