c2fb2a3241f09c1aa1c99b57359d8e012ddd6d3e
takaooo <takao@gladly.com>
2021-11-15 13:41:54 -0800

    fix tests + add custom

--- validator_test.go
+++ validator_test.go
@@ -28,7 +28,8 @@ type Schema struct {
 
 const (
 	draft7  = "draft7-tests"
-	draft201909  = "draft7-tests"
+	draft201909  = "draft2019-09-tests"
+	custom = "custom-tests"
 	msg  = "incorrect validate"
 )
 
@@ -39,7 +40,7 @@ var (
 )
 
 func init() {
-	dir := draft7
+	dir := custom
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		initErr = err
@@ -141,10 +142,7 @@ func BenchmarkSanthosh(b *testing.B) {
 		for _, s := range schemas {
 			// local init
 			b.StopTimer()
-			bytez, err := json.Marshal(s.Schema)
-			if err != nil {
-				b.Fatal(err.Error())
-			}
+			schemaJSON, err := json.Marshal(s.Schema)
 			if err != nil {
 				b.Fatal(err.Error())
 			}
@@ -152,18 +150,19 @@ func BenchmarkSanthosh(b *testing.B) {
 			for _, test := range s.Tests {
 				// Compiler doesn't handle schemas from these well, intercepting to get PASS
 				if
+				s.src == "refRemote.json" ||
+					s.src == "vocabulary.json" ||
 					s.src == "ref.json" {
 					continue
 				}
 				compiler := santhosh.NewCompiler()
 				compiler.Draft = santhosh.Draft2019
-				if err := compiler.AddResource("", bytes.NewReader(bytez)); err != nil {
-					b.Fatal(err.Error())
+				if err := compiler.AddResource("", bytes.NewReader(schemaJSON)); err != nil {
+					b.Fatal("failed to compile1!" + err.Error() + s.src)
 				}
 				schema, err := compiler.Compile("")
-
 				if err != nil {
-					b.Fatal(err.Error())
+					b.Fatal("failed to compile! " + s.src + "===" + err.Error())
 				}
 				err = schema.Validate(test.Data)
 				if (err == nil) != test.Valid {
@@ -173,6 +172,131 @@ func BenchmarkSanthosh(b *testing.B) {
 						s.Description,
 						test.Description)
 				}
+			}
+		}
+	}
+}
+
+func BenchmarkQriWithoutCompiler(b *testing.B) {
+	if initErr != nil {
+		b.Fatal(initErr.Error())
+	}
+	for i := 0; i < b.N; i++ {
+		for _, s := range schemas {
+			// local init
+			b.StopTimer()
+			bytes, err := json.Marshal(s.Schema)
+			if err != nil {
+				b.Fatal(err.Error())
+			}
+			qri.LoadDraft2019_09()
+			rs := new(qri.Schema)
+			err = json.Unmarshal(bytes, rs)
+			if err != nil {
+				b.Fatal(err.Error())
+			}
+			b.StartTimer()
+			for _, test := range s.Tests {
+				vs := rs.Validate(ctx, test.Data)
+				b.StopTimer()
+				if len(*vs.Errs) > 0 == test.Valid {
+					b.Logf("%s. schema file: %s. schema desc: %s. test desc: %s.",
+						msg,
+						s.src,
+						s.Description,
+						test.Description)
+				}
+				b.StartTimer()
+			}
+		}
+	}
+}
+
+func BenchmarkXeipuuWithoutCompiler(b *testing.B) {
+	if initErr != nil {
+		b.Fatal(initErr.Error())
+	}
+	for i := 0; i < b.N; i++ {
+		for _, s := range schemas {
+			b.StopTimer()
+			if
+			s.src == "refRemote.json"{
+				continue
+			}
+			schemaLoader := xeipuuv.NewGoLoader(s.Schema)
+			b.StartTimer()
+			for _, test := range s.Tests {
+				b.StopTimer()
+				if s.Description == "Location-independent identifier" ||
+					s.Description == "$anchor inside an enum is not a real identifier" {
+					continue
+				}
+				documentLoader := xeipuuv.NewGoLoader(test.Data)
+				schema, err := xeipuuv.NewSchema(schemaLoader)
+				if err != nil {
+					b.Fatal(err.Error())
+				}
+				b.StartTimer()
+				result, err := schema.Validate(documentLoader)
+				if err != nil {
+					b.Fatal(err.Error())
+				}
+				b.StopTimer()
+				if result.Valid() != test.Valid {
+					b.Logf("%s. schema file: %s. schema desc: %s. test desc: %s.",
+						msg,
+						s.src,
+						s.Description,
+						test.Description)
+				}
+				b.StartTimer()
+			}
+		}
+	}
+}
+
+func BenchmarkSanthoshWithoutCompiler(b *testing.B) {
+	if initErr != nil {
+		b.Fatal(initErr.Error())
+	}
+	for i := 0; i < b.N; i++ {
+		for _, s := range schemas {
+			// local init
+			b.StopTimer()
+			schemaJSON, err := json.Marshal(s.Schema)
+			if err != nil {
+				b.Fatal(err.Error())
+			}
+			b.StartTimer()
+			for _, test := range s.Tests {
+				b.StopTimer()
+				// Compiler doesn't handle schemas from these well, intercepting to get PASS
+				if
+				s.src == "refRemote.json" ||
+					s.src == "vocabulary.json" ||
+					s.src == "ref.json" {
+					continue
+				}
+				compiler := santhosh.NewCompiler()
+				compiler.Draft = santhosh.Draft2019
+				if err = compiler.AddResource("", bytes.NewReader(schemaJSON)); err != nil {
+					b.Fatal(err.Error())
+				}
+				var schema *santhosh.Schema
+				if schema, err = compiler.Compile(""); err != nil {
+					b.Fatal(err.Error())
+				}
+				b.StartTimer()
+				err = schema.Validate(test.Data)
+				b.StopTimer()
+				if (err == nil) != test.Valid {
+					b.Logf("%s. schema file: %s. schema desc: %s. test desc: %s.",
+						msg,
+						s.src,
+						s.Description,
+						test.Description)
+				}
+				b.StartTimer()
 			}
 		}
 	}
